e86000f88caee10beec09e5b4dceace70879cba7
ayaanza29 <91754746+ayaanza29@users.noreply.github.com>
2022-01-24 14:39:42 -0500

    class structure

--- final_format/QC.r
+++ final_format/QC.r
@@ -3,3 +3,18 @@ library(methods)
 things <- function(stuff) {
     print(5)
 }
+
+
+
+  
+movies <- setRefClass("movies", fields = list(name = "character", 
+                       leadActor = "character", rating = "numeric"), methods = list(
+                       increment_rating = function()
+                       {
+                           rating <<- rating + 1
+                       },
+                       decrement_rating = function()
+                       {
+                           rating <<- rating - 1
+                       }
+                     ))
--- final_format/main.r
+++ final_format/main.r
@@ -1,3 +1,4 @@
 library(methods)
 
-
+movieList <- movies(name = "Iron Man", 
+                    leadActor = "Robert downey Jr", rating = 7)
