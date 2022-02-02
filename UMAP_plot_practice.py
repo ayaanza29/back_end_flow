27e383670100ebe44dd0331361d091d5ba1856dc
ayaanza29 <91754746+ayaanza29@users.noreply.github.com>
2022-02-02 14:47:48 -0500

    finished plot changes

--- UMAP_plot_practice.py
+++ UMAP_plot_practice.py
@@ -36,6 +36,7 @@
 
 #################     Above works I think but takes forever
 
+from tokenize import String
 from bokeh.plotting import *
 from bokeh.models import *
 
@@ -97,20 +98,52 @@ from random import random
 from bokeh.layouts import row
 from bokeh.models import ColumnDataSource, CustomJS
 from bokeh.plotting import figure, output_file, show
+import FlowCal
+import matplotlib.pyplot as plt
+import holoviews
 
 output_file("callback.html")
 
-x = [random() for x in range(500)]
-y = [random() for y in range(500)]
+# x = [random() for x in range(500)]
+# y = [random() for y in range(500)]
+
+fcsFile_filtered = "C:/Users/Zuhayr/Documents/GitHub/r_background_app/PeacoQC_results/fcs_files/776 F SP_QC.fcs"
+s = FlowCal.io.FCSData(fcsFile_filtered)[:10000]
+x = s[:, ['FSC-A']] 
+y = s[:, ['FSC-H']]
+
 
 s1 = ColumnDataSource(data=dict(x=x, y=y))
-p1 = figure(width=400, height=400, tools="lasso_select, box_select, poly_select", title="Select Here")
+p1 = figure(width=400, height=400, tools="lasso_select, box_select, poly_select, pan, wheel_zoom, box_zoom, reset", title="Select Here")
+p1.xaxis.axis_label = 'FSC-A'
+p1.yaxis.axis_label = 'FSC-H'
 p1.circle('x', 'y', source=s1, alpha=0.6)
 
+# s2 = ColumnDataSource(data=dict(x=[], y=[]))
+# p2 = figure(width=400, height=400, x_range=(0, 1), y_range=(0, 1),
+#             tools="", title="Watch Here")
+# p2.circle('x', 'y', source=s2, alpha=0.6)
+
 s2 = ColumnDataSource(data=dict(x=[], y=[]))
-p2 = figure(width=400, height=400, x_range=(0, 1), y_range=(0, 1),
-            tools="", title="Watch Here")
+p2 = figure(width=400, height=400, tools="pan, wheel_zoom", title="Watch Here")
+p2.xaxis.axis_label = 'FSC-A'
+p2.yaxis.axis_label = 'FSC-H'
 p2.circle('x', 'y', source=s2, alpha=0.6)
+
+
+# x3 = s[:, ['SSC-A']] 
+# y3 = s[:, ['SSC-H']]
+# s3 = ColumnDataSource(data=dict(x=x3, y=y3))
+# p3 = figure(width=400, height=400, tools="lasso_select, box_select, poly_select, pan, wheel_zoom, box_zoom, reset", title="Select Here")
+# p3.circle('x', 'y', source=s1, alpha=0.6)
+
+x3 = s[:, ['FSC-A']] 
+y3 = s[:, ['FSC-W']]
+s3 = ColumnDataSource(data=dict(x=x3, y=y3))
+p3 = figure(width=400, height=400, tools="lasso_select, box_select, poly_select, pan, wheel_zoom, box_zoom, reset", title="Select Here")
+p3.xaxis.axis_label = 'FSC-A'
+p3.yaxis.axis_label = 'FSC-W'
+p3.circle('x', 'y', source=s3, alpha=0.6)
 
 s1.selected.js_on_change('indices', CustomJS(args=dict(s1=s1, s2=s2), code="""
         const inds = cb_obj.indices;
@@ -126,6 +159,15 @@ s1.selected.js_on_change('indices', CustomJS(args=dict(s1=s1, s2=s2), code="""
     """)
 )
 
-layout = row(p1, p2)
+# fcsFile = "C:/Users/Zuhayr/Downloads/776 F SP.fcs"
+# fcsFile ="C:/Users/Zuhayr/Documents/GitHub/r_background_app/PeacoQC_results/fcs_files/776 F SP_QC.fcs"
+# s = FlowCal.io.FCSData(fcsFile)
+# s = FlowCal.transform.to_rfi(s)
+# FlowCal.plot.density2d(s, channels=["FSC-A", "SSC-A"], mode = "scatter")
+# plt.show()
+
+layout = row(p1, p2, p3)
 
 show(layout)
+
+save(layout)
